82024b1d667a3d2558e5f4bc33e74d4ca65a0554
DewhiteE <dewhiteeofficial@gmail.com>
2022-06-07 19:27:16 +0300

    tmp update

--- Source/ModularBuildSystem/Private/ModularBuildSystemActor.cpp
+++ Source/ModularBuildSystem/Private/ModularBuildSystemActor.cpp
@@ -408,7 +408,7 @@ FModularSectionActor AModularBuildSystemActor::InitModularSectionActor(const FTr
 		InLevelId, Sections.SpawnNewSectionActor(InTransform, InClass, SpawnConfiguration.SectionSpawnParams));
 
 	checkf(NewSectionActor.IsValid(), TEXT("%s: NewSectionActor was invalid: %s, InClass=%s, bAddToActorSections=%s, bWithRelativeTransform=%s"), 
-		*GetName(), *NewSectionActor.ToString(), *InClass->GetName(),
+		*GetName(), *NewSectionActor.ToString(), InClass ? *InClass->GetName() : *FName().ToString(),
 		bAddToActorSections ? TEXT("true") : TEXT("false"),
 		bWithRelativeTransform ? TEXT("true") : TEXT("false"));
 	AttachActor(NewSectionActor.GetActor(), bWithRelativeTransform);
